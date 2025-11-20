aea08aab59e372d5c02e78b8ae850c5a28675d19
Xiomara-Estevez <xiomaraestevez01@gmail.com>
2025-11-20 18:11:43 -0500

    Upload from Brightspace Companion: Chess.h

---
+++ 2025-11-20T23-11-43-105Z-Chess.h
@@ -0,0 +1,14 @@
+#ifndef Chess_h
+#define Chess_h
+#include <iostream>
+
+class Chess
+{
+    public:
+            // enum 3 types of players:
+    enum players {player1, player2, computer};
+
+};
+using namespace std;
+
+#endif
